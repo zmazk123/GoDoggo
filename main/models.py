b1a07f5227401ae1bf294a04b537135da2cd7be8
Marko Zmazek <markozmazek123@gmail.com>
2021-12-03 01:34:11 +0100

    Demo read/write from firebase

--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import pyrebase
+from pyrebase.pyrebase import Database
 
-# Create your models here.
+config = {
+    "apiKey": "AIzaSyAgaZYQDBNyfMNI3A7ocJB1DP_vHiUdo2o",
+    "authDomain": "godoggo-a18ac.firebaseapp.com",
+    "databaseURL": "https://godoggo-a18ac-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "godoggo-a18ac",
+    "storageBucket": "godoggo-a18ac.appspot.com",
+    "messagingSenderId": "929780314718",
+    "appId": "1:929780314718:web:31b97491b560bc07dc7acf"
+}
+
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
+
+class Entry(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+
+    def __init__(self):
+        self.name = ""
+        self.surname = ""
+        
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def save(self):
+        data = {"name": self.name, "surname": self.surname}
+        database.child("Entries").push(data)
+
+    @staticmethod
+    def getEntries():
+        return database.child("Entries").get().val()
--- main/views.py
+++ main/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import pyrebase
+from pyrebase.pyrebase import Database
+from .forms import CreateNewList
+from .models import Entry
 
-# Create your views here.
+config = {
+    "apiKey": "AIzaSyAgaZYQDBNyfMNI3A7ocJB1DP_vHiUdo2o",
+    "authDomain": "godoggo-a18ac.firebaseapp.com",
+    "databaseURL": "https://godoggo-a18ac-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "godoggo-a18ac",
+    "storageBucket": "godoggo-a18ac.appspot.com",
+    "messagingSenderId": "929780314718",
+    "appId": "1:929780314718:web:31b97491b560bc07dc7acf"
+}
+
+firebase = pyrebase.initialize_app(config)
+authe = firebase.auth()
+database = firebase.database()
+
+def index(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            surname = form.cleaned_data["surname"]
+            newEntry = Entry(name, surname)
+            newEntry.save()
+
+    allEntries = Entry.getEntries()
+    #entryOneName = allEntries['1']['name']
+    form = CreateNewList()
+    return render(response, "main/home.html", {"entries":allEntries, "form":form})
