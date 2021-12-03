dd8242b8b60f18aa97d0218b43aaf844a19aa376
Marko Zmazek <markozmazek123@gmail.com>
2021-12-03 01:51:44 +0100

    Base

--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import pyrebase
 
-# Create your views here.
+config = {
+    "apiKey": "AIzaSyAgaZYQDBNyfMNI3A7ocJB1DP_vHiUdo2o",
+    "authDomain": "godoggo-a18ac.firebaseapp.com",
+    "databaseURL": "https://godoggo-a18ac-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "godoggo-a18ac",
+    "storageBucket": "godoggo-a18ac.appspot.com",
+    "messagingSenderId": "929780314718",
+    "appId": "1:929780314718:web:31b97491b560bc07dc7acf"
+}
+
+firebase = pyrebase.initialize_app(config)
+authe = firebase.auth()
+
+def index(response):
+    return render(response, "main/home.html")
